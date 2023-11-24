5e9d935a27988e7ea13501fc2df9af02bc31847d
ACodrin <100201180+ACodrin@users.noreply.github.com>
2023-11-24 11:27:05 +0200

    Update S1-2.py

--- Partial/S1-2.py
+++ Partial/S1-2.py
@@ -25,18 +25,3 @@ cpd_R2 = TabularCPD(variable='R2', variable_card=3,
                             [0,     0,      0.25, ]],
                     evidence=['J', 'R1'],
                     evidence_card=[2, 2])
-
-
-cpd_c = TabularCPD(variable='C', variable_card=2, values=[[0.9995], [0.0005]]) # C=0 cutremurul nu are loc, C=1 invers
-
-cpd_i = TabularCPD(variable='I', variable_card=2,
-                   values=[[0.99, 0.97],
-                           [0.01, 0.03]],
-                    evidence=['C'],
-                    evidence_card=[2]) # I=0 incendiul nu are loc, I=1 invers
-
-cpd_a = TabularCPD(variable='A', variable_card=2,
-                   values=[[0.9999, 0.98, 0.05, 0.02],
-                           [0.0001, 0.02, 0.95, 0.98]],
-                    evidence=['C', 'I'],
-                    evidence_card=[2, 2]) # A=0 ialarma nu se declanseaza, A=1 invers 
