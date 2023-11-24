1f6d6840c1d092de663ce105d6ca2c663110ea14
ACodrin <100201180+ACodrin@users.noreply.github.com>
2023-11-24 11:39:10 +0200

    Update S1-2.py

--- Partial/S1-2.py
+++ Partial/S1-2.py
@@ -7,9 +7,8 @@ import networkx as nx
 # J - primul jucator
 # R1 - prima runda
 # R2 - a doua runda
-# C - castigator
 
-model = BayesianNetwork([('J', 'R1'), ('J', 'R2'), ('R1', 'R2'), ('R1', 'C'), ('R2', 'C')])
+model = BayesianNetwork([('J', 'R1'), ('J', 'R2'), ('R1', 'R2')])
 
 cpd_J = TabularCPD(variable='J', variable_card=2, values=[[0.5], [0.5]])
 
@@ -20,8 +19,20 @@ cpd_R1 = TabularCPD(variable='R1', variable_card=2,
                     evidence_card=[2])
 
 cpd_R2 = TabularCPD(variable='R2', variable_card=3,
-                    values=[[1/3,   0.5,    0.25, ],
-                            [2/3,   0.5,    0.5, ],
-                            [0,     0,      0.25, ]],
+                    values=[[1/3,   0.5,    1/9,    0.25],
+                            [2/3,   0.5,    4/9,    0.5],
+                            [0,     0,      4/9,    0.25]],
                     evidence=['J', 'R1'],
                     evidence_card=[2, 2])
+
+model.add_cpds(cpd_J, cpd_R1, cpd_R2)
+
+assert model.check_model()
+
+infer = VariableElimination(model)
+result = infer.query(variables=['J'], evidence={'R2': 1})
+print(result)
+
+pos = nx.circular_layout(model)
+nx.draw(model, pos=pos, with_labels=True, node_size=4000, font_weight='bold', node_color='skyblue')
+plt.show()
